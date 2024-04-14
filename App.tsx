2838d6e92ead2bfa03e8f82a498c0124c89398d2
xvax <95727642+xvax00@users.noreply.github.com>
2024-04-14 11:45:24 +0900

    added

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Hello from "./src/compo/Hello";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello hiki="World" bang={true}></Hello>
+      <Hello hiki="World" bang={false} style={{ fontSize: 16 }}></Hello>
       <Text>Open up App.tsx to start working on your app!!!</Text>
       <StatusBar style="auto" />
     </View>
