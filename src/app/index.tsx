7889bcba3b447e5daee5bb11501293fe76829910
xvax <95727642+xvax00@users.noreply.github.com>
2024-04-15 11:57:19 +0900

    aaa

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+const Index = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Hello WOrld!</Text>
+    </View>
+  );
+};
+
+export default Index;
